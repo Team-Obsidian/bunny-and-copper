3f9272807ce274c93465bad45b83237f5a3a6a41
Team-Obsidian <78922515+Team-Obsidian@users.noreply.github.com>
2025-07-04 11:59:23 -0500

    atk timers, inv frames

--- main.lua
+++ main.lua
@@ -29,25 +29,33 @@ function love.load()
 		item.y = item.y or 0
 		item.width = item.width or 100
 		item.height = item.height or 100
-		item.radius = item.radius or 50
+		item.radius = item.radius or 30
 		item.speed = item.speed or 300
 		item.id = item.id or 0
 		item.hit = item.hit or false
+		item.hitTimer = item.hitTimer or 0
+		item.hitTimerMax = item.hitTimerMax or 3
 		table.insert(chr_All, item)
 	end
 
 	function genAtk(item)
 		item.x = item.x or 0
 		item.y = item.y or 0
+		item.velX = item.velX or 0
+		item.velY = item.velY or 0
 		item.width = item.width or 100
 		item.height = item.height or 100
 		item.radius = item.radius or 50
 		item.speed = item.speed or 300
 		item.id = item.id or 0
 		item.renderType = item.renderType or 'circle'
+		-- despawns after some condition or specified time
+		item.timeOut = item.timeOut or nil
+		item.clear = item.clear or false
 		table.insert(atk_All, item)
 	end
 end
+
 
 function love.update(dt)
 	pressing = {} -- Initialize key presses only once
@@ -79,33 +87,60 @@ function love.update(dt)
 	end
 
 	for i, atk in pairs(atk_All) do
-		-- To do: Check for time increment before despawn
-
-		-- Check if hitbox overlap
+		-- Check if hitbox overlap, (note: comes before time & clear command)
 		for j, chr in pairs(chr_All) do
-			if atk.radius + chr.radius > distTo(atk, chr) then
-				print('hit')
+
+			if atk.radius + chr.radius > distTo(atk, chr) and chr.hit == false then
+				--print('hit')
 				chr.hit = true
+				chr.hitTimer = chr.hitTimerMax
 			else
 				--print('not hit')
-				chr.hit = false
+				--chr.hit = false
 			end
 		end
+
+		if atk.timeOut ~= nil then
+			if atk.timeOut < 0 then 
+				atk.clear = true
+			else
+				atk.timeOut = atk.timeOut - dt
+			end 
+		end
+
+		if atk.clear == true then
+			atk_All[i] = nil
+		end
 	end
+
+	for i, chr in pairs(chr_All) do
+		if chr.hitTimer < 0 then
+			chr.hit = false
+			chr.hitTimer = 0
+		elseif chr.hit == true then
+			chr.hitTimer = chr.hitTimer - dt
+		end
+	end
+
+
 end
 
 function love.keypressed(key, scancode, isrepeat)
 	if key == 'o' then
-		genChr{}
+		genAtk{x=love.math.random(0, 1200),y=love.math.random(0,720),timeOut=3}
 	elseif key == 'p' then
 		genChr{id=1}
 	elseif key == 'u' then
 		genAtk{x=600,y=350}
+	elseif key == 'backspace' then
+		chr_All = {}
+		atk_All = {}
 	end
 end
 
 function love.draw()
 	--love.graphics.print("Hello World!", 400, 300)
+
 	for i, chr in pairs(chr_All) do
 		if chr.id == 1 then
 			if chr.hit == true then
@@ -113,15 +148,33 @@ function love.draw()
 			elseif chr.hit == false then
 				love.graphics.setColor(1, 0.2, 0.3)
 			end
+		else
+			love.graphics.setColor(0.1, 0.7, 0.2)	
 		end
 		renderCir(chr)	
 		love.graphics.setColor(1, 1, 1)		
+		love.graphics.print('chr_id: ' .. chr.id, chr.x, chr.y)
 	end
 
 	for i, atk in pairs(atk_All) do
 		if atk.renderType == 'circle' then
+			love.graphics.setColor(0, 0.4, 0.7)
 			love.graphics.circle('fill', atk.x, atk.y, atk.radius)
+
 		end
+
+		love.graphics.setColor(1, 1, 1)		
+		love.graphics.print('atk_id: ' .. atk.id, atk.x, atk.y)
 	end
 
+
+	for i, chr in pairs(chr_All) do
+		if chr.id == 1 then
+			temp = 0
+			for j, value in pairs(chr) do
+				love.graphics.print(tostring(j) .. ': ' .. tostring(value), 1200, 20+20*temp)
+				temp = temp + 1
+			end
+		end 
+	end
 end
